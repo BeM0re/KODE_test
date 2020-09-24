8a7db6df8ff63c63ba6625843ff38d8ea8aad7a3
Vitaly <zmei@2ch.hk>
2020-09-24 22:23:58 +0700

    add fullscreen fragment w/o downloading

--- app/src/main/java/ru/be_more/kode_test/presentation/fullscreen/FullscreenFragment.kt
+++ app/src/main/java/ru/be_more/kode_test/presentation/fullscreen/FullscreenFragment.kt
@@ -1,6 +1,38 @@
 package ru.be_more.kode_test.presentation.fullscreen
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import kotlinx.android.synthetic.main.fragment_fullscreen.*
+import ru.be_more.kode_test.R
 
 class FullscreenFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? =
+        inflater.inflate(R.layout.fragment_fullscreen, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initView(view)
+    }
+
+    private fun initView(view: View) {
+
+        val url = requireArguments().getString("url")
+
+        Glide.with(view)
+            .load(url)
+            .diskCacheStrategy(DiskCacheStrategy.ALL)
+            .into(iv_fullscreen_photo)
+    }
+
 }
